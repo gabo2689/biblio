3fb0c30025ab483b9884826b1c454e0178890e75
Gabriel Recinos <gabrielhh26@gmail.com>
2015-03-25 02:32:15 -0600

    salvando

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,28 @@ class Order < ActiveRecord::Base
   validates_presence_of :quality
   validates_numericality_of :quality, greater_than: 0
 
-  before_validation :set_total
+  around_create :set_total
 
   def set_total
     self.total = self.book.price * self.quality
   end
+
+  def decorate_total
+    "Lps #{total}"
+  end
+
+
+  def decorate_fecha
+    created_at.strftime(" %d %b %Y %l:%M %p")
+  end
+
+  def decorate_client
+    if self.client.present?
+      self.client
+    else
+      "Anonimo"
+    end
+  end
+
 
 end
