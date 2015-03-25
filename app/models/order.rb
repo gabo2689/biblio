9e8c354461a1ebbd788445dd10df6016cb9078d6
Gabriel Recinos <gabrielhh26@gmail.com>
2015-03-25 01:50:28 -0600

    salvando

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,10 @@ class Order < ActiveRecord::Base
   validates_presence_of :quality
   validates_numericality_of :quality, greater_than: 0
 
+  before_validation :set_total
+
+  def set_total
+    self.total = self.book.price * self.quality
+  end
 
 end
